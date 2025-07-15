6d997ae2b43215fc14bef9a98447bd7966fe4e8b
valentinaroxo <roxovalentina@gmail.com>
2025-07-15 10:49:13 -0300

    Versão com botão limpar

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // Primeiro, precisamos de uma referência ao nosso elemento <textarea>.
     // Usamos 'document.getElementById' para pegar o elemento pelo 'id' que definimos no HTML.
     const blocoDeNotas = document.getElementById('blocoDeNotas');
+    const btnLimparNotas = document.getElementById('btnLimparNotas');
+    //adicionamos um evento de click a botão para limpar as notas
+    btnLimparNotas.addEventListener('click', () => {
+        //quando o botão é clicado, limpamos o conteúdo do bloco de notas
+        blocoDeNotas.value = '';
+        //e também removemos a nota salva do localStorage
+        localStorage.removeItem('minhaNota');
+        console.log("Notas limpas e removidas do localStorage!");
+    });
+    
 
     // 2. CARREGANDO DADOS DO LOCALSTORAGE
     // ------------------------------------
